5ee89568a79522a86e0ef3cacbac8c52cb018b89
Simon Deutsch <simon.deutsch97@gmail.com>
2021-09-27 16:03:25 +0200

    * added obfuscator to production build

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,7 +1,28 @@
 /* eslint-disable import/no-extraneous-dependencies */
 const { merge } = require('webpack-merge');
+const WebpackObfuscator = require('webpack-obfuscator');
 const common = require('./webpack.common');
 
 module.exports = merge(common, {
   mode: 'production',
+  plugins: [
+    new WebpackObfuscator({
+      rotateStringArray: true,
+    }),
+  ],
+  // webpack loader rules array
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        enforce: 'post',
+        use: {
+          loader: WebpackObfuscator.loader,
+          options: {
+            rotateStringArray: true,
+          },
+        },
+      },
+    ],
+  },
 });
