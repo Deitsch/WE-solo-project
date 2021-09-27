e02f2624348f7355a22f0607ad4feccf7584e2e8
Simon Deutsch <simon.deutsch97@gmail.com>
2021-09-27 15:43:54 +0200

    * fixed linter

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,3 @@
-/* eslint-disable import/newline-after-import, eol-last */
 import './styles/style.scss';
 
 console.log('Hello npm project!');
--- webpack.dev.js
+++ webpack.dev.js
@@ -1,6 +1,7 @@
+/* eslint-disable import/no-extraneous-dependencies */
 const path = require('path');
 const { merge } = require('webpack-merge');
-const common = require('./webpack.common.js');
+const common = require('./webpack.common');
 
 module.exports = merge(common, {
   mode: 'development',
